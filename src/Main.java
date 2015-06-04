eed61eeb19cb2a78634ee3f0f79d68c4da72b395
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-06-04 14:34:01 +0900

    Solve AOJ 2002

--- src/Main.java
+++ src/Main.java
@@ -6,134 +6,102 @@ import java.math.BigInteger;
 /**
  * @author yoshikyoto
  */
-class Main {
+class Main extends MyUtil{
 	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(new InputStreamReader(System.in));
-		
-		while(true){	
-			int n = sc.nextInt();
-			if(n == 0) break;
-			
-			boolean[][] schedule = new boolean[n][30];
-			
-			for(int i = 0; i < n; i++){
-				int m = sc.nextInt();
-				for (int j = 0; j < m; j++) {
-					schedule[i][sc.nextInt() - 1] = true;
-				}
+		int n = readInt();
+		for (int i = 0; i < n; i++) {
+			int h = readIntMap(0);
+			int w = readIntMap(1);
+			char[][] c = new char[h][];
+			for (int j = 0; j < h; j++) {
+				c[j] = readLine().toCharArray();
 			}
-			
-			System.out.println(solve(n, schedule));
+			if(solve(c, h, w)){
+				System.out.println("SAFE");
+			}else{
+				System.out.println("SUSPICIOUS");
+			}
 		}
 	}
 	
-	static int solve(int n, boolean[][] schedule){
-		boolean[][] canHave = new boolean[n][n];
-		for (int i = 0; i < n; i++) {
-			canHave[i][i] = true;
+	static boolean solve(char[][] c, int h, int w){
+		while(checkThisTurn(c, h, w));
+		// 全部消えたかチェック
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if(c[i][j] != '.' && c[i][j] != '*'){
+					return false;
+				}
+			}
 		}
-		
-		for (int day = 0; day < 30; day++) {
-			// day日にaさんとbさんの予定が合う場合は地図を交換できる
-			for (int a = 0; a < n; a++) {
-				for (int b = 0; b < a; b++) {
-					if(schedule[a][day] && schedule[b][day]){
-						// 地図をシェアする
-						share(n, canHave, a, b);
+		return true;
+	}
+	
+	static boolean checkThisTurn(char[][] c, int h, int w){
+		HashSet<Character> checked = new HashSet<Character>();
+		checked.add('.');
+		checked.add('*');
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if(!checked.contains(c[i][j])){
+					if(checkRect(c, h, w, c[i][j])){
+						// 長方形だったら
+						return true;
+					}else{
+						// 長方形でなかったら
 					}
 				}
+				// このターンでは確認済み
+				checked.add(c[i][j]);
 			}
-			
-			// 地図は集まったかどうかをチェックする
-			if(check(n, canHave)){
-				return day + 1;
-			}
-		}
-		return -1;
-	}
-	
-	static void share(int n, boolean[][] canHave, int a, int b){
-		for (int i = 0; i < n; i++) {
-			boolean tmp = canHave[a][i] || canHave[b][i];
-			canHave[a][i] = tmp;
-			canHave[b][i] = tmp;
-		}
-	}
-	
-	/**
-	 * 誰か一人でも、地図を全部集められる人がいたらtrue
-	 */
-	static boolean check(int n, boolean[][] canHave){
-		for (int person = 0; person < n; person++) {
-			if(check(n, person, canHave)) return true;
 		}
 		return false;
 	}
 	
-	/**
-	 * person番目の人が地図を全部集められるならtrue
-	 */
-	static boolean check(int n, int person, boolean[][] canHave){
-		for (int i = 0; i < n; i++) {
-			if(!canHave[person][i]) return false;
+	static boolean checkRect(char[][] c, int h, int w, char target){
+		int top = h, bottom = 0, left = w, right = 0;
+		// 上下左右の端を確認
+		for (int i = 0; i < h; i++) {
+			for (int j = 0; j < w; j++) {
+				if(c[i][j] == target){
+					top = Math.min(top, i);
+					bottom = Math.max(bottom, i);
+					left = Math.min(left, j);
+					right = Math.max(right, j);
+				}
+			}
+		}
+		
+		// そこが埋まっているか確認
+		if(isFilled(c, h, w, target, top, bottom, left, right)){
+			//埋まってる
+			for(int i = top; i <= bottom; i++){
+				for(int j = left; j <= right; j++){
+					// 次からワイルとカードとして扱える
+					c[i][j] = '*';
+				}
+			}
+			return true;
+		}else{
+			//埋まってない
+			return false;
+		}
+	}
+	
+	static boolean isFilled(char[][] c, int h, int w, int target,
+			int top, int bottom, int left, int right){
+		for(int i = top; i <= bottom; i++){
+			for(int j = left; j <= right; j++){
+				if(c[i][j] != target && c[i][j] != '*'){
+					return false;
+				}
+			}
 		}
 		return true;
 	}
 }
 
-/**
- * UnionFindTree 
- * @author yoshikyoto
- */
-class UnionFindTree {
-	public int[] parent, rank;
-	public int n;
-	public int count;
 
-	// 初期化
-	UnionFindTree(int n) {
-		this.n = n;
-		count = n;
-		parent = new int[n];
-		rank = new int[n];
-		for (int i = 0; i < n; i++) {
-			parent[i] = i;
-			rank[i] = 0;
-		}
-	}
-
-	// 根を求める
-	int find(int x) {
-		if (parent[x] == x) {
-			return x;
-		} else {
-			return parent[x] = find(parent[x]);
-		}
-	}
-
-	// xとyの集合を結合
-	void unite(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if (x == y) {
-			return;
-		}
-		if (rank[x] < rank[y]) {
-			parent[x] = y;
-			count--;
-		} else {
-			parent[y] = x;
-			if (rank[x] == rank[y])
-				rank[x]++;
-			count--;
-		}
-	}
-
-	// xとyが同じ集合か
-	boolean same(int x, int y) {
-		return find(x) == find(y);
-	}
-}
 
 class BinaryIndexedTree{
     int n;
