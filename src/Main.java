483738c939def7868acb814017157b9fd1235a77
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-06-04 22:06:32 +0900

    Paticipate topcoder.SRM660Div2

--- src/Main.java
+++ src/Main.java
@@ -3,101 +3,15 @@ import java.util.*;
 import java.lang.ArrayIndexOutOfBoundsException;
 import java.math.BigInteger;
 
+import topcoder.SRM660Div2.PrivateD2party;
+
 /**
  * @author yoshikyoto
  */
 class Main extends MyUtil{
 	public static void main(String[] args) throws Exception{
-		int n = readInt();
-		for (int i = 0; i < n; i++) {
-			int h = readIntMap(0);
-			int w = readIntMap(1);
-			char[][] c = new char[h][];
-			for (int j = 0; j < h; j++) {
-				c[j] = readLine().toCharArray();
-			}
-			if(solve(c, h, w)){
-				System.out.println("SAFE");
-			}else{
-				System.out.println("SUSPICIOUS");
-			}
-		}
-	}
-	
-	static boolean solve(char[][] c, int h, int w){
-		while(checkThisTurn(c, h, w));
-		// 全部消えたかチェック
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if(c[i][j] != '.' && c[i][j] != '*'){
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	
-	static boolean checkThisTurn(char[][] c, int h, int w){
-		HashSet<Character> checked = new HashSet<Character>();
-		checked.add('.');
-		checked.add('*');
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if(!checked.contains(c[i][j])){
-					if(checkRect(c, h, w, c[i][j])){
-						// 長方形だったら
-						return true;
-					}else{
-						// 長方形でなかったら
-					}
-				}
-				// このターンでは確認済み
-				checked.add(c[i][j]);
-			}
-		}
-		return false;
-	}
-	
-	static boolean checkRect(char[][] c, int h, int w, char target){
-		int top = h, bottom = 0, left = w, right = 0;
-		// 上下左右の端を確認
-		for (int i = 0; i < h; i++) {
-			for (int j = 0; j < w; j++) {
-				if(c[i][j] == target){
-					top = Math.min(top, i);
-					bottom = Math.max(bottom, i);
-					left = Math.min(left, j);
-					right = Math.max(right, j);
-				}
-			}
-		}
-		
-		// そこが埋まっているか確認
-		if(isFilled(c, h, w, target, top, bottom, left, right)){
-			//埋まってる
-			for(int i = top; i <= bottom; i++){
-				for(int j = left; j <= right; j++){
-					// 次からワイルとカードとして扱える
-					c[i][j] = '*';
-				}
-			}
-			return true;
-		}else{
-			//埋まってない
-			return false;
-		}
-	}
-	
-	static boolean isFilled(char[][] c, int h, int w, int target,
-			int top, int bottom, int left, int right){
-		for(int i = top; i <= bottom; i++){
-			for(int j = left; j <= right; j++){
-				if(c[i][j] != target && c[i][j] != '*'){
-					return false;
-				}
-			}
-		}
-		return true;
+		int[] a = {5,2,2,4,5,0};
+		System.out.println(new PrivateD2party().getsz(a));
 	}
 }
 
---
+++ src/topcoder/SRM660Div2/Cyclemin.java
@@ -0,0 +1,33 @@
+package topcoder.SRM660Div2;
+
+public class Cyclemin {
+	public String bestmod(String s, int k){
+		int len = s.length();
+		String ans = null;
+		for (int i = 0; i < len; i++) {
+			char c = s.charAt(0);
+			s = s.substring(1) + c;
+			String replaced_s = replace(s, k);
+			// System.out.println(replaced_s);
+			if(ans == null){
+				ans = replaced_s;
+			}else if(ans.compareTo(replaced_s) > 0){
+				ans = replaced_s;
+			}
+		}
+		return ans;
+	}
+	
+	String replace(String s, int k){
+		if(k == 0) return s;
+		char[] c = s.toCharArray();
+		for(int i = 0; i < c.length; i++){
+			if(c[i] != 'a'){
+				c[i] = 'a';
+				k--;
+				if(k == 0) break;
+			}
+		}
+		return String.valueOf(c);
+	}
+}
