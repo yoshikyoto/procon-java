ee35b1d2e4e1e621bae370b31b74eec93ffbc118
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-06-14 19:28:38 +0900

    Solved

--- src/Main.java
+++ src/Main.java
@@ -7,57 +7,86 @@ import java.math.BigInteger;
  * @author yoshikyoto
  */
 class Main {
+	static Scanner sc = new Scanner(new InputStreamReader(System.in));
 	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(new InputStreamReader(System.in));
 		while(true) {
 			int n = sc.nextInt();
 			if(n == 0) break;
-			
-			int[] p = new int[n];
-			int[] t = new int[n];
-			for (int i = 0; i < n; i++) {
-				p[i] = sc.nextInt();
-				t[i] = sc.nextInt();
-			}
-			int inf = 1<<28;
-			int[] prev_distance = new int[4];
-			prev_distance[1] = p[0];
-			
-			for (int i = 1; i < n; i++) {
-				// i番目の風船
-				int[] next_distance = new int[4];
-				
-				for(int j = 1; j < 4; j++) {
-					if(prev_distance[j] == 0) {
-						// 家に戻るパターン
-						// 0に戻ってから風船の位置に
-						// 家に戻る
-						int distance = p[i-1];
-						int time = p[i-1] * (j+1);
-						// 風船のの位置へ
-						distance += p[i];
-						time += p[i];
-						if(time <= t[i]) {
-							update(next_distance, j, prev_distance[j] + distance);
-						}
-						
-						// 家に戻らないパターン
-						if(j != 3 && prev_distance[j] == 0) {
-							
-						}
-					}
-				}
-			}
+			String[] ans = solve(n);
+			System.out.println(ans[0] + " " + ans[1]);
 		}
 		sc.close();
 	}
 	
-	static void update (int[] a, int i, int val) {
-		if(a[i] == 0) {
-			a[i] = val;
-		}else{
-			a[i] = Math.min(a[i], val);
+	static String[] solve(int n) {
+		int[] p = new int[n];
+		int[] t = new int[n];
+		for (int i = 0; i < n; i++) {
+			p[i] = sc.nextInt();
+			t[i] = sc.nextInt();
 		}
+		int inf = 1<<28;
+		int[] prev_distance = new int[4];
+		Arrays.fill(prev_distance, inf);
+		
+		// 最初の風船
+		if(p[0] > t[0]){
+			String[] ans = {"NG", "1"};
+			return ans;
+		}
+		prev_distance[1] = p[0];
+		
+		
+		for (int i = 1; i < n; i++) {
+			if(getMin(prev_distance) == inf) {
+				String[] ans = {"NG", ""+i};
+				return ans;
+			}
+			
+			int[] next_distance = new int[4];
+			Arrays.fill(next_distance, inf);
+			
+			for(int j = 1; j < 4; j++) {
+				if(prev_distance[j] != inf) {
+					// 家に戻って風船に行くまでの距離
+					int distance = p[i-1] + p[i];
+					// 家に戻って風船に行くまでの時間
+					int time = p[i-1] * (j+1) + p[i];
+					// 間に合う場合は値を更新
+					if(t[i-1] + time <= t[i]) {
+						next_distance[1] = Math.min(next_distance[1], prev_distance[j] + distance);
+					}
+					
+					// 家に戻らないパターン
+					// 風船がいっぱいでない
+					if(j != 3) {
+						distance = Math.abs(p[i] - p[i-1]);
+						time = distance * (j+1);
+						if(t[i-1] + time <= t[i]) {
+							next_distance[j+1] = Math.min(next_distance[j+1], prev_distance[j] + distance);
+						}
+					}
+				}
+			}
+
+			prev_distance = next_distance;
+		}
+		int mindist = getMin(prev_distance);
+		if(mindist == inf) {
+			String[] ans = {"NG", ""+n};
+			return ans;
+		}else{
+			int dist = mindist + p[n-1];
+			String[] ans = {"OK", ""+dist};
+			return ans;
+		}
+	}
+	static int getMin(int[] a) {
+		int ans = 1<<28;
+		for (int i = 0; i < a.length; i++) {
+			ans = Math.min(ans, a[i]);
+		}
+		return ans;
 	}
 }
 
