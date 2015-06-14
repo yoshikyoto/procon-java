b46630c1ce92000ab43927a4017a65905f4e9848
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-06-14 23:46:23 +0900

    Solve 1248

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,81 @@
 import java.io.*;
 import java.util.*;
 import java.lang.ArrayIndexOutOfBoundsException;
-import java.math.BigInteger;
 
-/**
- * @author yoshikyoto
- */
+
 class Main {
+	static Scanner sc = new Scanner(new InputStreamReader(System.in));
 	public static void main(String[] args) throws Exception {
+		while(true) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int d = sc.nextInt();
+			if(a + b + d == 0) break;
+			int[] ans = solve(a, b, d);
+			System.out.println(ans[0] + " " + ans[1]);
+		}
+	}
+	
+	static void comp(int[] ans, int acount, int bcount) {
+		if(ans[0] + ans[1] > acount + bcount) {
+			ans[0] = acount;
+			ans[1] = bcount;
+		}
+	}
+	
+	static int[] solve(int a, int b, int d) {
+		int inf = 1 << 28;
+		int[] ans = {inf, inf};
+		
+		int acnt = d / a;
+		acnt++;
+		int bcnt = 0;
+		while(acnt >= 0) {
+			int sum = a * acnt + b * bcnt;
+			if(sum == d) {
+				comp(ans, acnt, bcnt);
+				acnt--;
+			} else if(sum > d) {
+				acnt--;
+			} else {
+				bcnt++;
+			}
+		}
+		
+		int asum = a;
+		int bsum = 0;
+		while(asum != bsum) {
+			int diff = asum - bsum;
+			if(Math.abs(diff) == d) {
+				comp(ans, asum / a, bsum / b);
+				break;
+			}
+			if(diff < d) {
+				asum += a;
+			} else {
+				bsum += b;
+			}
+		}
+		
+		asum = 0;
+		bsum = b;
+		while(asum != bsum) {
+			int diff = bsum - asum;
+			if(Math.abs(diff) == d) {
+				comp(ans, asum / a, bsum / b);
+				break;
+			}
+			if(diff < d) {
+				bsum += b;
+			} else {
+				asum += a;
+			}
+		}
+		
+		return ans;
 	}
 }
+
 
 /**
  * 複素数
