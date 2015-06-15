56508a1ef247ccd246dca371313fc614676e256b
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2015-06-15 23:58:01 +0900

    Solve 1194

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,21 @@ import java.math.*;
 class Main {
 	static Scanner sc = new Scanner(new InputStreamReader(System.in));
 	public static void main(String[] args) throws Exception {
-		int r = sc.nextInt();
-		int n = sc.nextInt();
-		System.out.printf("%.6f", solve(r, n));
+		try{
+		while(true) {
+			int r = sc.nextInt();
+			int n = sc.nextInt();
+			if(r+n == 0) break;
+			System.out.printf("%.4f\n", solve(r, n));
+		}
+		} catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 	
+	
 	static double solve(int r, int n) {
-
-		int[] dp = new int[39];
-		HashMap<int[], Integer> map = new HashMap<int[], Integer>();
+		int[] dp = new int[40];
 		for (int i = 0; i < n; i++) {
 			int xl = sc.nextInt();
 			int xr = sc.nextInt();
@@ -26,40 +32,25 @@ class Main {
 			}
 		}
 		
-		for(double ans = -r; ans < 20.0; ans += 0.001) {
+		for(double ans = -r; ans < 20.0; ans += 0.0005) {
 			for (int i = 1-r; i < r; i++) {
 				int index = i + 20;
 				int h = Math.min(dp[index-1], dp[index]);
-				double dist = sqdist(0.0, ans, (double)h, 0.0);
-				if(dist < (double)(r*r)) {
+				double dist = sqdist(0.0, ans, (double)i, (double)h);
+				if(dist <= (double)(r*r)) {
 					return ans + (double)r;
 				}
 			}
 		}
 		return 0.0;
 	}
-	public static int sq(int i) {
-		return i * i;
-	}
-
+	
 	public static double sq(double d) {
 		return d * d;
 	}
-	// square を snippet から実装してください
-	public static int sqdist(int x1, int y1, int x2, int y2) {
-		return sq(x1 - x2) + sq(y1 - y2);
-	}
-
+	
 	public static double sqdist(double x1, double y1, double x2, double y2) {
 		return sq(x1 - x2) + sq(y1 - y2);
-	}
-
-	public static double dist(int x1, int y1, int x2, int y2) {
-		return Math.sqrt(sqdist(x1, y1, x2, y2));
-	}
-
-	public static double dist(double x1, double y1, double x2, double y2) {
-		return Math.sqrt(sqdist(x1, y1, x2, y2));
 	}
 }
 
